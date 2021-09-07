6db9b664d61d4bfd30517fe13a285d3af5ac5144
Sunil <sunilnet4@gmail.com>
2021-09-08 00:37:45 +0530

    wip

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 class App extends React.Component {
     render(){
         return(
-            <h1>Hello Flashtype App!</h1>
+            <div className="app">
+                <p>The App</p>
+            </div>
         )
     }
 }
