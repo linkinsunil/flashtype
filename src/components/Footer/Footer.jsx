25b872c16f353bb5f20c7e909983bc119a8218f1
Sunil <sunilnet4@gmail.com>
2021-09-08 20:03:23 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <a href="https://www.github.com/linkinsunil" className="footer-link"
+            target="_blank"
+            rel="noreferrer"
+            >
+                View on Github            
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
