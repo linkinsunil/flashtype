eb17d951b070b86caab865ff3d1baa95ec753af8
Sunil <sunilnet4@gmail.com>
2021-09-08 13:57:50 +0530

    wip

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
 import './App.css';
 
 class App extends React.Component {
     render(){
         return(
             <div className="app">
-                <p>The App</p>
+                <Nav />
             </div>
         )
     }
