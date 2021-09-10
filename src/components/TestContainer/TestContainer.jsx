21ccb7dafb5852ca1bcb537959b021533e8e2dcb
Sunil <sunilnet4@gmail.com>
2021-09-10 11:56:58 +0530

    wip

--- src/components/TestContainer/TestContainer.jsx
+++ src/components/TestContainer/TestContainer.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import TryAgain from '../TryAgain/TryAgain';
 import TypingChallengeContainer from '../TypingChallengeContainer/TypingChallengeContainer';
-import './TestContainer.css'
+import './TestContainer.css';
+
+const timeRemaining = 30;
 
 const TestContainer = ({ words, characters, wpm }) => {
     return (
         <div className="test-container">
-            <div data-aos="fade-up" className="typing-challenge-container">
-                <TypingChallengeContainer 
-                    words={words}
-                    characters={characters}
-                    wpm={wpm}
-                />
-            </div>
-            {/* <div className="try-again-container">
-                <TryAgain words={words} characters={characters} wpm={wpm} />
-            </div> */}
+            {timeRemaining > 0
+                ? <div data-aos="fade-up" className="typing-challenge-container">
+                    <TypingChallengeContainer
+                        words={words}
+                        characters={characters}
+                        wpm={wpm}
+                    />
+                </div>
+                : <div className="try-again-container">
+                    <TryAgain words={words} characters={characters} wpm={wpm} />
+                </div>}
         </div>
     )
 }
